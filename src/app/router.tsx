ae4161646ab918ceee255139b4b3f848d18ca47c
Ha Truong <hacaotantruong0@gmail.com>
2025-12-18 21:04:10 +0700

    thêm file ProductDetail.tsx

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,8 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '../pages/Home/HomePage';
 import LoginPage from '../components/Auth/Login/LoginPage';
 import RegisterPage from '../components/Auth/Register/RegisterPage';
+import ProductDetail from "../pages/productDetail/ProductDetail.tsx";
 
 export const router = createBrowserRouter([
   {
