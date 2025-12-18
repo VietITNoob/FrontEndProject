6c456cd5fd52e1b9346eea4539ccd065f7b3b314
HoangVu-Nguyen <hoangvufit2004@gmail.com>
2025-12-18 18:12:06 +0700

    feat(login and register):update component login ang register

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from '../pages/Home/HomePage';
+import LoginPage from '../components/Auth/Login/LoginPage';
+import RegisterPage from '../components/Auth/Register/RegisterPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
   },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+
 ]);
