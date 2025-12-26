6d05e2d253ff32228fc0da466097761564ea091e
Viet <kiritoak01@gmail.com>
2025-12-26 22:12:06 +0700

    call api Lấy và hiển thị sản phẩm cho trang giỏ hàng

---
+++ src/service/cartService.tsx
@@ -0,0 +1,22 @@
+import type { Product } from '../types';
+import axiosClient from "../api/axiosClient.tsx";
+
+
+export interface CartItem extends Product {
+  quantity: number;
+}
+
+export const cartService = {
+  getCartItems: (): Promise<CartItem[]> => {
+    return axiosClient.get('/cart');
+  },
+  addToCart: (product: Product): Promise<CartItem> => {
+
+
+    const cartItem = { ...product, quantity: 1 };
+    return axiosClient.post('/cart', cartItem);
+  },
+  removeFromCart: (cartItemId: string | number): Promise<void> => {
+    return axiosClient.delete(`/cart/${cartItemId}`);
+  }
+};
