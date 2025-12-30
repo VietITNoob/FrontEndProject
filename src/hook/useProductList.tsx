e6f7c980bd7a5581621f1fddb67721feff76c0ba
Ha Truong <hacaotantruong0@gmail.com>
2025-12-31 02:11:31 +0700

    tách các lớp xử lý trong component ra thành các hook

---
+++ src/hook/useProductList.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import { productService } from '../service/productService';
+import type {Product} from '../types';
+
+
+export const useProductList = () => {
+    const [all, setAll] = useState<Product[]>([]);
+    const [byCategory, setByCategory] = useState<Product[]>([]);
+    const [bestSellers, setBestSellers] = useState<Product[]>([]);
+    const [newProducts, setNewProducts] = useState<Product[]>([]);
+    const [topRatedProducts, setTopRatedProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const [allProds, best, newProds, topRated] = await Promise.all([
+                    productService.getAll(),
+                    productService.getBestsale(),
+                    productService.getNewProduct(),
+                    productService.getRatingProduct()
+                ]);
+                setAll(allProds);
+                setBestSellers(best);
+                setNewProducts(newProds);
+                setTopRatedProducts(topRated);
+            } catch (err) {
+                setError("Error fetching products");
+                console.error("Error fetching products:", err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const fetchByCategory = async (categoryId: string) => {
+        try {
+            setLoading(true);
+            const data = await productService.getBycategory(categoryId);
+            setByCategory(data);
+        } catch (err) {
+            setError("Error fetching products by category");
+            console.error("Error fetching products by category:", err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { all, byCategory, bestSellers, newProducts, topRatedProducts, loading, error, fetchByCategory };
+};
