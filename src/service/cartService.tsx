10f789e4bada18ae9f644849bbe1a75d70a4156b
Viet <kiritoak01@gmail.com>
2026-01-01 15:07:57 +0700

    update số lượng của cùng 1 sản phẩm khi thêm vào giỏ hàng

--- src/service/cartService.tsx
+++ src/service/cartService.tsx
@@ -11,10 +11,11 @@ export const cartService = {
     return axiosClient.get('/cart');
   },
   addToCart: (product: Product): Promise<CartItem> => {
-
-
     const cartItem = { ...product, quantity: 1 };
     return axiosClient.post('/cart', cartItem);
+  },
+  updateCartItem: (item: CartItem): Promise<CartItem> => {
+    return axiosClient.put(`/cart/${item.id}`, item);
   },
   removeFromCart: (cartItemId: string | number): Promise<void> => {
     return axiosClient.delete(`/cart/${cartItemId}`);
